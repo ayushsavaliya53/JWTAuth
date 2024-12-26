ea8c34159355e71235124a5d21b5d3e6217fd549
ayushsavaliya53 <ayushsavaliya53@gmail.com>
2024-12-26 19:39:39 +0530

    removed databse url package to avoid build error

--- JWTAUth/settings.py
+++ JWTAUth/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import datetime
 from environ import Env
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -96,7 +95,7 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(env('DATABASE_URL')),
+    'default': env('DATABASE_URL'),
 }
 
 # Password validation
